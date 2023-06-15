dec296d326d49e1c5fbac0e90937946f4e8e4eca
wilsonz0 <105890511+wilsonz0@users.noreply.github.com>
2023-06-15 11:30:06 -0400

    changed into a function

--- check.py
+++ check.py
@@ -1,6 +1,11 @@
 from mcstatus import JavaServer
 from config import ADDRESS
 
-server = JavaServer.lookup(ADDRESS)
-status = server.status()
-print(f"The server has {status.players.online} player(s) online")
+
+def get_number_online():
+    server = JavaServer.lookup(ADDRESS)
+    status = server.status()
+
+    # print(f"The server has {status.players.online} player(s) online")
+    return status.players.online
+    
