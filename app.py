7701da5093a60b0778d1dd20923acd6f8761b416
wilsonz0 <105890511+wilsonz0@users.noreply.github.com>
2023-06-15 00:24:07 -0400

    added a way to retrieve the number of players online

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 from mcstatus import JavaServer
 from config import ADDRESS
+
+server = JavaServer.lookup(ADDRESS)
+status = server.status()
+print(f"The server has {status.players.online} player(s) online")
