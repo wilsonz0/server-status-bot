79d04de2afa633d7c799050fbd90ed67de98204b
wilsonz0 <105890511+wilsonz0@users.noreply.github.com>
2023-06-25 23:09:24 -0400

    allowed status to be retrieved every 30 mins given a way to start and stop

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import responses
+from discord.ext import tasks
 from config import TOKEN 
 
 async def send_message(message, user_message, is_private):
@@ -18,6 +19,7 @@ def run_discord_bot():
     @client.event
     async def on_ready():
         print(f'{client.user} is now running')
+        # send_update.start()
 
 
     @client.event
@@ -25,16 +27,28 @@ def run_discord_bot():
         if message.author == client.user:
             return
         
+        # send_update.stop()
+        
         username = str(message.author)
         user_message = str(message.content)
 
         print(f'{username} said "{user_message}"')
 
-        if user_message[0] == '?':
+        if user_message == '!startLoop':
+            send_update.start()
+        elif user_message == '!endLoop':
+            send_update.stop()
+        elif user_message[0] == '?':
             user_message = user_message[1:]
             await send_message(message, user_message, is_private=True)
         else:
             await send_message(message, user_message, is_private=False)
+
+    @tasks.loop(minutes=30)
+    async def send_update():
+        response = responses.get_response("!getstatus")
+        print(response)
+
 
     client.run(TOKEN)
 
